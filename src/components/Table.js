3fe09c3920384168c7d80d46eb230ea42e20e89b
admindavidson <adekoyaemmanuel762@gmail.com>
2024-01-26 16:08:46 +0100

    complete api call

---
+++ src/components/Table.js
@@ -0,0 +1,17 @@
+function Table(){
+
+    return(
+    <table className='table table-striped'>
+    <tr>
+        <th>S/N</th>
+        <th>Name</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Emmanuel</td>
+    </tr>
+    </table>
+    )
+}
+
+export default Table
--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import logo from './logo.svg'
+// import App from './App';
+// import Table from './components/Table'
+
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// const appname = 'Devconf'
+// const message = ReactDOM.createRoot(document.getElementById('banner'))
+// // let obj = {
+// //   border:"2px solid orange",
+// //   backgroundColor:"lightblue",
+// //   backgroundImage: `url(${'bgimage'})`
+// //   };
+
+
+
+// message.render(
+//   <React.StrictMode>
+//     <h1 className="alert alert-danger">Hello World {appname.toUpperCase()}</h1>
+//     <img className='img-fluid' alt="" style={{border:"2px solid orange", backgroundColor:"lightblue"}} src={logo} onClick={()=>alert('hello')}/>
+//   </React.StrictMode>
+//  )
+
+//  const news = ReactDOM.createRoot(document.getElementById('news'))
+
+// news.render(
+//   <Table/>
+// )
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <App/>
+)
+
+
+// const msg = <h2>Welocme to My Site</h2>
+
+// const banner = ReactDOM.createRoot(document.getElementById('banner'));
+// let name = 'bayo'
+// banner.render(<p>This is the banner {name}</p>)
+
+// root.render(
+//   <React.StrictMode>
+//     {msg}
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
